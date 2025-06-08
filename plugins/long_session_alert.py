8bc3f3c94fb8303c036fe5a964d255d589f14f63
Pavlo Gorosko <pavlogorosko56@gmail.com>
2025-06-08 11:22:26 +0200

    s

--- plugins/long_session_alert.py
+++ plugins/long_session_alert.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from plyer import notification
 
 class Plugin:
@@ -9,20 +8,12 @@ class Plugin:
 
     def handle_event(self, event, data):
         if event == "app_switched":
-            if self.current_app and self.current_duration >= 1800:  /
-
-=======
-from plyer import notification
-
-class Plugin:
-    def __init__(self, app):
-        self.app = app
-        self.current_app = None
-        self.current_duration = 0
-
-    def handle_event(self, event, data):
-        if event == "app_switched":
-            if self.current_app and self.current_duration >= 1800:  /
-
->>>>>>> 3ffa54c606a27bf104e1f5386be6a21cce76d47c
-System: * Today's date and time is 09:13 AM CEST on Saturday, June 14, 2025.
+            if self.current_app and self.current_duration >= 1800:
+                # Add your notification logic here
+                notification.notify(
+                    title="Long Session Alert",
+                    message=f"You have used {self.current_app} for more than 30 minutes.",
+                    timeout=10
+                )
+            self.current_app = data.get("app_name")
+            self.current_duration = 0
