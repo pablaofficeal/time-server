32bd472d3b35e060b55866c2ee557e60c8b2187e
pablaofficeal <pavlogorosko56@gmail.com>
2025-06-08 11:20:11 +0200

    Update long_session_alert.py

--- plugins/long_session_alert.py
+++ plugins/long_session_alert.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from plyer import notification
 
 class Plugin:
@@ -10,8 +9,6 @@ class Plugin:
     def handle_event(self, event, data):
         if event == "app_switched":
             if self.current_app and self.current_duration >= 1800:  /
-
-=======
 from plyer import notification
 
 class Plugin:
@@ -23,6 +20,4 @@ class Plugin:
     def handle_event(self, event, data):
         if event == "app_switched":
             if self.current_app and self.current_duration >= 1800:  /
-
->>>>>>> 3ffa54c606a27bf104e1f5386be6a21cce76d47c
 System: * Today's date and time is 09:13 AM CEST on Saturday, June 14, 2025.
