b1a862dedc8419f3c479a884cf9bcddd813ce66f
pabla officeal <pavlogorosko56@gmail.com>
2025-10-09 23:00:32 +0200

    main start

--- main/claint_server/logs_server.py
+++ main/claint_server/logs_server.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify
-
-app = Flask(__name__)
 from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    return jsonify({"message": "Logs server is running"})
+
+
+
+if __name__ == "__main__":
+    app.run(host=app.config["SERVER_IP"], port=app.config["SERVER_PORT"])
