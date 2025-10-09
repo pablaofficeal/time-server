d869335f19797537b309e7fca6970b8c2414aae3
pabla officeal <pavlogorosko56@gmail.com>
2025-10-09 22:18:37 +0200

    main start

--- powershell/test2.ps1
+++
@@ -1,9 +0,0 @@
-$paths = @(
-    "$env:TEMP\*",
-    "$env:WINDIR\Temp\*",
-    "$env:LOCALAPPDATA\Temp\*"
-)
-foreach ($path in $paths) {
-    Remove-Item $path -Recurse -Force -ErrorAction SilentlyContinue
-}
-Write-Output "Очистка завершена!"
--- powershell/test3.ps1
+++
@@ -1,5 +0,0 @@
-if (Test-Connection -ComputerName google.com -Count 1 -Quiet) {
-    Write-Output "There is internet :)"
-} else {
-    Write-Output "There is no internet :("
-}
